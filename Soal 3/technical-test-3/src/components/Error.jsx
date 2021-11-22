586259b4c44dcf85f21ef3c4a3de28202e47d4e9
Shendy <shendyppy@gmail.com>
2021-11-22 18:11:47 +0700

    refactor no. 3

---
+++ Soal 3/technical-test-3/src/components/Error.jsx
@@ -0,0 +1,11 @@
+function Error() {
+	return (
+		<>
+			<div className="container mx-auto text-center mt-10 mb-10">
+				<h1>Something Went Wrong!</h1>
+			</div>
+		</>
+	);
+}
+
+export default Error;
